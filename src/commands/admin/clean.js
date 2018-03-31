ff9c40517e0af7e19f74a4ddee9728159ca09495
JEstes93 <jestes93jobs@gmail.com>
2018-03-31 19:29:30 -0400

    Refactored some code Added soft-cap for messages to delete, for friendliness with Discord API Added workaround allowing bulk deletion of messages older than 14 days

--- src/commands/admin/clean.js
+++ src/commands/admin/clean.js
@@ -4,26 +4,29 @@ module.exports = {
         if (msg.member.hasPermission("ADMINISTRATOR")) {
             if (clargs.length < 1) return;
 
-            msg.reply("Are you sure?").then(reply => {
-                reply.react('✅');
+            const num = Math.min(parseInt(clargs[0], 10), 100);
+            const addin = (num === 100) ? ` (Note: Limit is 100, greater values will be reduce to match)` : '';
 
-                reply.createReactionCollector((reaction, user) =>
-                    (reaction.emoji.name === '✅' && !user.bot && user.id === msg.author.id),
-                    { time: 5000, }
-                ).on('collect', (reaction, collection) => collection.stop('success')
-                ).on('end', (collected, reason) =>
-                    ((reason === 'success')
-                        ? msg.channel.fetchMessages({
-                            limit: parseInt(clargs[0], 10),
-                            before: msg.id,
-                        }).then(data => msg.channel.bulkDelete(data))
-                        : reply.edit("Action cancelled due to timeout!")
-                    ).then(() => {
-                        msg.delete();
-                        reply.delete();
-                    })
-                );
-            });
+            msg.reply(`**Are you sure?**${addin}`)
+                .then(reply => reply.react('✅')
+                    .then(reaction => reaction.message)
+                    .then(reply => reply.createReactionCollector((reaction, user) =>
+                        (reaction.emoji.name === '✅' && !user.bot && user.id === msg.author.id),
+                        { time: 5000, }
+                    ).on('collect', (reaction, collection) => collection.stop('success')
+                    ).on('end', (collected, reason) =>
+                        ((reason === 'success')
+                            ? msg.channel.fetchMessages({
+                                limit: num,
+                                before: msg.id,
+                            }).then(data => msg.channel.bulkDelete(data)
+                                .then(res => res, reason => data.deleteAll()))
+                            : reply.edit("Action cancelled due to timeout!")
+                        ).then(() => {
+                            msg.delete();
+                            reply.delete();
+                        }).catch(console.error)
+                    )));
         }
     },
     description: "Allows admins to bulk-delete messages in channels"
